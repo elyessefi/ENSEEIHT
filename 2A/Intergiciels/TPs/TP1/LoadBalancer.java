2ca1f3522ba6bec1089442d736d46dbaa5465d8f
elyessefi <68329525+elyessefi@users.noreply.github.com>
2023-01-11 18:15:59 +0100

    Update LoadBalancer.java

--- 2A/Intergiciels/TPs/TP1/LoadBalancer.java
+++ 2A/Intergiciels/TPs/TP1/LoadBalancer.java
@@ -1,1 +1,28 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.util.Random;
 
+public class LoadBalancer {
+    static String hosts[] = {"localhost", "localhost"};
+    static int ports[] = {8081,8082};
+    static int nbHosts = 2;
+    static Random rand = new Random();
+    
+    public static void main(String[] args) throws IOException{
+        ServerSocket server = new ServerSocket(8080);
+        while(true){
+            try {   
+                    Socket ss = server.accept();
+                    Slave slave = new Slave(ss);
+                    slave.run();
+            }catch(IOException e){break;}
+        }
+    }
+}
